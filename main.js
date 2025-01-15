1fe31be791685f8a4a1d5a3897a9e2918e462428
Mehul Rana <mehulrana412@gmail.com>
2025-01-15 16:55:15 -0600

    added ally vanguard quotes

---
+++ main.js
@@ -0,0 +1,35 @@
+let ally = document.querySelector('#ally-quote');
+let enemy = document.querySelector('#enemy-quote');
+let character = document.querySelector('#character');
+let quote = document.querySelector('#quote');
+
+const allyQuotes = [{
+  quote: '"Together! To victory!"',
+  character: 'Captain America'
+}, {
+  quote: '"I unleash the Vishantis power.',
+  character: 'Doctor Strange',
+}, {
+  quote: '"We are Groot!"',
+  character: 'Groot',
+}, {
+  quote: '"My turn now!"',
+  character: 'Bruce Banner'
+}, {
+  quote: '"NOW HULK MAD!"',
+  character: 'Hulk'
+}, {
+  quote: '"For Mutankind!"',
+  character: 'Magneto'
+}, {
+  quote: '"Spider mode!"',
+  character: 'Peni Parker'
+}, {
+  quote: `"I am Asgard's MIGHT!"`,
+  character: 'Thor'
+}, {
+  quote: '"We hunger!"',
+  character: 'Venom'
+}]
+
+// dialogues: https://dotesports.com/marvel/news/all-marvel-rivals-ultimate-voice-lines
